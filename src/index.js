2b5edacc3db11d02fb57592b5bdf25cadd44e474
K-mBT <k.beattie87@gmail.com>
2024-09-14 14:49:38 +0930

    added in the AI API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-function generatePoem(event) {
+function displayPoem(response) {
   event.preventDefault();
 
   new Typewriter("#poem", {
-    strings: "La tombe dit à la rose",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  let apiKey = "2046c535afeb092fo82f1d306d8a2b2t";
+  let context =
+    "You are a french poem writer, specialising in short poems written in french language. The poem must be provided in HTML format. Example: <p>this is a poem</p>";
+  let prompt = "Generate a short and unique poem in french. ";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+
+  poemElement.innerHTML = "Generating a poem for you.. please wait";
+
+  console.log("called the AI api");
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
