f176b10f2bb7eb077f138dd9e1605da4625198e7
K-mBT <k.beattie87@gmail.com>
2024-09-14 15:26:00 +0930

    changed it to german poem

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "t177fb2e6d160a85cb2o5b8390de4f39";
   let context =
-    "You are french poet. You must generate a 5 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Do not refer to HTML in your poem.";
-  let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
+    "You are german poet. You must generate a 5 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Do not refer to HTML in your poem.";
+  let prompt = `User instructions: Generate a German poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a German poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
